8726875e858c6f40cb8eb2fe4e1035a62015a1a2
Danny <dannvanos93@gmail.com>
2020-10-05 12:35:38 +0200

    Create the logger with user input

---
+++ logger.ps1
@@ -0,0 +1,31 @@
+$hostToPing = Read-Host -Prompt 'Welke locatie wil je loggen?'
+$logPathLocation = "$HOME\Desktop\ping-logger"
+$logPath = "$HOME\Desktop\ping-logger\pinglog-$hostToPing.txt"
+$alwaysTrue = 1
+
+if(!(Test-Path $logPathLocation)){
+    New-Item -ItemType Directory -Force -Path $logPathLocation
+}
+
+while($alwaysTrue -eq "1")
+{
+
+        # refresh the timestamp before each ping attempt
+        $theTime = Get-Date -format g
+
+        # refresh the ping variable
+        $result = ping $hostToPing -n 1
+
+                if ($result -like '*reply*')
+                {
+                    Write-Output "$theTime - pass - connection to $hostToPing is up" | Out-File $logPath -append
+                }
+                else
+                {
+                    Write-Output "$theTime - fail - connection to $hostToPing is down" | Out-File $logPath -append
+                }
+
+        Sleep 1
+        echo "Pinging $hostToPing"
+
+}
